23039ad3b356f0566879d112ca071f0ece44a212
Viktor Froberg <vikfroberg@gmail.com>
2019-04-11 20:51:19 +0200

    make it even prettier

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ app.get("*", (req, res, next) => {
             <meta charset="utf-8">
             <title>${path}</title>
             <style>
+              @-webkit-keyframes pulse {
+                0% {
+                  background: #2e83e3;
+                }
+                100% {
+                  background: #88b4e7;
+                }
+              }
+              @keyframes pulse {
+                0% {
+                  background: #2e83e3;
+                }
+                100% {
+                  background: #88b4e7;
+                }
+              }
               body {
                 margin: 0;
                 padding: 40px;
@@ -46,24 +62,36 @@ app.get("*", (req, res, next) => {
                 background: #f2f1f6;
               }
               .header {
-                border: 1px solid #f2f1f6;
+                border-bottom: 10px solid #f2f1f6;
                 padding: 24px;
                 font-size: 18px;
                 font-family: menlo;
                 font-weight: bold;
-                border-bottom-width: 4px;
                 background: #fff;
               }
               .header__home {
+                display: inline-block;
+                text-decoration: none;
                 color: #2e83e3;
               }
-              .header__path {
+              .header__home:hover {
+                text-decoration: underline;
+              }
+              .header__seg {
+                display: inline-block;
+                text-decoration: none;
+                color: #303030;
+              }
+              .header__seg:hover {
+                text-decoration: underline;
+              }
+              .header__sep {
+                display: inline-block;
                 color: #303030;
               }
               .list {
                 margin: 0;
                 padding: 0;
-                border: 1px solid #f2f1f6;
                 background: #fff;
               }
               .list__item--file {
@@ -71,6 +99,10 @@ app.get("*", (req, res, next) => {
                 padding: 0;
                 list-style: none;
                 border-bottom: 1px solid #f2f1f6;
+              }
+              .list__item--file svg {
+                vertical-align: middle;
+                width: 28px;
               }
               .list__item--dir {
                 margin: 0;
@@ -80,6 +112,7 @@ app.get("*", (req, res, next) => {
               }
               .list__item--dir svg {
                 vertical-align: middle;
+                width: 28px;
               }
               .list__item--file:last-child,
               .list__item--dir:last-child {
@@ -99,12 +132,30 @@ app.get("*", (req, res, next) => {
                 color: #fff;
                 fill: #fff;
               }
+              .list__item_link.clicked,
+              .list__item_link.clicked:hover {
+                color: #fff;
+                -webkit-animation: pulse 1s ease-in-out infinite alternate;
+                animation: pulse 1s ease-in-out infinite alternate;
+              }
             </style>
           </head>
           <body>
             <div class="header">
-              <span class="header__home">~</span>
-              <span class="header__path">${path}</span>
+              <a href="/" class="header__home">~</a>
+              <span class="header__sep">/</span>
+              ${path
+                .split("/")
+                .slice(1)
+                .map((seg, i) => {
+                  const href = path
+                    .split("/")
+                    .slice(1, i + 1)
+                    .concat([seg])
+                    .join("/");
+                  return `<a class="header__seg" href="/${href}">${seg}</a>`;
+                })
+                .join(`\n<span class="header__sep">/</span>\n`)}
             </div>
             <ul class="list">
               ${files
@@ -112,13 +163,21 @@ app.get("*", (req, res, next) => {
                 .filter(file => fs.lstatSync("." + file.path).isDirectory())
                 .map(file => {
                   return `
-                      <li class="list__item--dir">
-                        <a class="list__item_link" href="${file.path}">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                          ${file.name}
-                        </a>
-                      </li>
-                    `;
+                    <li class="list__item--dir">
+                      <a class="list__item_link" href="${file.path}">
+                        <svg id="Layer_1" version="1" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
+                          <style>
+                            .st4{fill:none;stroke:#444b54;stroke-width:6;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10}
+                          </style>
+                          <path d="M109 19H24c-3 0-5 2-5 5v85c0 3 2 5 5 5h85c3 0 5-2 5-5V24c0-3-2-5-5-5z" fill="#fff"/>
+                          <path d="M96 59H59L49 49H24c-3 0-5 2-5 5v55c0 3 2 5 5 5h77V64c0-3-3-5-5-5z" fill="#fff0b3"/>
+                          <path d="M39 109c0 3 2 5 5 5h57v-10H44c-3 0-5 2-5 5z" fill="#f2b630"/>
+                          <path class="st4" d="M107 114H24c-3 0-5-2-5-5V54c0-3 2-5 5-5h22l5 2 6 6 5 2h34c2 0 5 2 5 5v43c0 4 3 7 6 7 4 0 7-3 7-7V59M114 59V24c0-3-2-5-5-5H24c-3 0-5 2-5 5v16"/>
+                        </svg>
+                        ${file.name}
+                      </a>
+                    </li>
+                  `;
                 })
                 .join("\n")}
               ${files
@@ -126,18 +185,38 @@ app.get("*", (req, res, next) => {
                 .filter(file => fs.lstatSync("." + file.path).isFile())
                 .map(file => {
                   return `
-                      <li class="list__item--file">
-                        <a class="list__item_link" href="${file.path}">
-                          ${file.name}
-                        </a>
-                      </li>
-                    `;
+                    <li class="list__item--file">
+                      <a class="list__item_link" href="${file.path}">
+                        <svg id="Layer_1" version="1" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
+                          <style>
+                            .st8{fill:#c3dbea}
+                          </style>
+                          <path d="M99 114H29c-3 0-5-2-5-5V19c0-3 2-5 5-5h55l20 20v75c0 3-2 5-5 5z" fill="#fff"/>
+                          <path d="M95 37H81V14H29c-3 0-5 2-5 5v90c0 3 2 5 5 5h70c3 0 5-2 5-5V34L84 14h-3" fill="none" stroke="#444b54" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+                          <path class="st8" d="M79 79H49a15 15 0 1 0 30 0z"/>
+                          <circle class="st8" cx="49" cy="59" r="8"/>
+                          <path class="st8" d="M83 62h-9c-2 0-3-1-3-3s1-3 3-3h9c2 0 3 1 3 3s-1 3-3 3z"/>
+                        </svg>
+                        ${file.name}
+                      </a>
+                    </li>
+                  `;
                 })
                 .join("\n")}
             </ul>
+            <script type="text/javascript">
+              document.querySelectorAll(".list__item_link").forEach(function(element) {
+                element.addEventListener("click", function(event) {
+                  event.target.classList.add("clicked");
+                });
+              });
+            </script>
           </body>
         </html>
-      `;
+      `
+        .split("\n")
+        .map(s => s.trim())
+        .join("\n");
       res.end(html);
     } else if (stat.isFile()) {
       const ext = Path.extname(path);
@@ -300,7 +379,7 @@ function handleCompilerError(err, stats) {
   }
 }
 
-app.use("/", express.static("./"));
+app.use("/", express.static("./", { dotfiles: "allow" }));
 
 app.listen(port, () =>
   console.log(
